b3157a171d0f2cd9e84cf53af1cb9a76c63ef7c1
Matthew Croall <phoozle@phoozle-desktop.(none)>
2010-09-06 22:21:52 +0930

    More work on Midi model

--- app/models/midi.rb
+++ app/models/midi.rb
@@ -4,12 +4,43 @@ class Midi
   def self.all
     Dir.chdir PATH_TO_MIDIS
     files = Dir.glob('*.mid')
-    files.map! {|f| File.new f}
+    files.map! {|f| new(f)}
   end
 
   def self.find(param)
     param.gsub!(/.mid/, '')
     Dir.chdir PATH_TO_MIDIS
-    file = File.new "#{param}.mid"
+    file = new("#{param}.mid")
+  end
+
+  def name
+    File.basename(@file)
+  end
+
+  def initialize(midi)
+    @file = File.new(midi)
+    @seq = MIDI::Sequence.new
+    @seq.read(@file)
+  end
+
+  def bpm
+    @seq.bpm
+  end
+
+  def tracks
+    tracks = []
+    @seq.each do |t|
+      track = {:name => t.name, :instrument => t.instrument}
+      tracks << track
+    end
+  tracks
+  end
+
+  def mpq
+    MIDI::Tempo.bpm_to_mpq(self.bpm)
+  end
+
+  def measures
+    @seq.get_measures
   end
 end
