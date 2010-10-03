855e0581cc52e74d0df7b0c5771ca87b476a6f29
Matthew Croall <phoozle@phoozle-desktop.(none)>
2010-10-03 17:16:24 +1030

    Added support for midi download with Mime type.

--- app/controllers/midis_controller.rb
+++ app/controllers/midis_controller.rb
@@ -1,10 +1,16 @@
 class MidisController < ApplicationController
+
   def index
     @midis = Midi.all
   end
 
   def show
     @midi = Midi.where(:name => params[:id]).first
+    
+    respond_to do |format|
+      format.html
+      format.mid { send_data @midi.data, :type => 'audo/midi', :disposition => 'inline' }
+    end
   end
 
   def new
